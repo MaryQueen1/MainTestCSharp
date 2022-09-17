85628b3ba021831dcc2686f97b9d127ae279aaee
Mary <maryqueenk1@gmail.com>
2022-09-17 19:51:35 +0300

    Создание метода для формирования массива строк

--- Итоговая проверочная работа/Program.cs
+++ Итоговая проверочная работа/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array = CreateArray();
+
+string[] CreateArray()
+{
+    Console.WriteLine("Введите матрицу: ");
+    string? Text = Console.ReadLine();
+    Text = Text.Replace(",", "");
+    Text = Text.Replace(".", "");
+
+    string[] array = Text.Split(" ");
+    string[] ar = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        ar[i] = Convert.ToString(array[i]);
+    }
+    return ar;
+}
