ecbb9df66e41a6c3573ea821a8e4122fedd298df
Mary <maryqueenk1@gmail.com>
2022-09-19 22:40:16 +0300

    решение поставленной задачи

--- Итоговая проверочная работа/Program.cs
+++ Итоговая проверочная работа/Program.cs
@@ -1,5 +1,7 @@
 ﻿Start();
 string[] Array = CreateArray();
+string str = string.Join(", ", Array);
+Console.Write($"[{str}] -> ");
 ShortWord(Array);
 
 void Start()
@@ -10,21 +12,35 @@ void Start()
 
 string[] CreateArray()
 {
-    Console.WriteLine("Введите длину массива: ");
-    int Length = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите массив: ");
+    string? Text = Console.ReadLine();
+    Text = Text.Replace(",", "");
+    Text = Text.Replace(".", "");
 
-    for (int i = 0; i < Length - 1; i++)
+    string[] array = Text.Split(" ");
+    string[] ar = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
     {
-        System.Console.WriteLine($"Введите {i + 1} набор символов: ");
-        string? Array[i + 1]
+        ar[i] = Convert.ToString(array[i]);
     }
+    return ar;
 }
 
 void ShortWord(string[] Array)
 {
-    int max = 3;
     for (int i = 0; i < Array.Length; i++)
     {
-
+        if (Array[i].Length <= 3)
+        {
+            if (i != Array.Length - 1)
+            {
+                Console.Write($"{Array[i]}, ");
+            }
+            else
+            {
+                Console.Write($"{Array[i]}");
+            }
+        }
     }
 }
